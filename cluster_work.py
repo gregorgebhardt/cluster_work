18db4e6d9011a6d05aeff02e6c106909520e3f5e
Gregor Gebhardt <gregor.gebhardt@gmail.com>
2018-03-23 00:20:19 +0100

    fixed bug: encoding of string to hash.

--- cluster_work.py
+++ cluster_work.py
@@ -597,7 +597,7 @@ class ClusterWork(object):
         self._log_path_rep = os.path.join(config['log_path'], '{:02d}'.format(rep), '')
         self._plotting = config['plotting'] if 'plotting' in config else True
         self._no_gui = (not config['gui'] if 'gui' in config else False) or self.__runs_on_cluster or self._NO_GUI
-        self._seed_base = zlib.adler32(self._name) % int(1e6)
+        self._seed_base = zlib.adler32(self._name.encode()) % int(1e6)
         self._seed = self._seed_base + 1000 * rep
 
         # set params of this repetition
